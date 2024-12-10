3d96bfe7f3256000bb0c7366e3cad497710d37cd
webdev-akshay <webdev.akshay@gmail.com>
2024-12-11 00:10:22 +0530

    joke app complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { JokeService } from './service/joke.service';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'joke-telling-bot-app';
+  jokes:any=[]=[]
+  constructor(private  JokeService:JokeService){}
+
+  getJoke(){
+    this.JokeService.getJoke().subscribe((data)=>{
+      this.jokes=data;
+    })
+  }
+  ngOnInit(): void {
+    this.getJoke()
+  }
 }
